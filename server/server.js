d6d0575959df36387df553cabbf8dcd42ff092ec
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-03-11 09:35:47 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
