d1bdeddcb8b4780f087f8ae177e0aca486a31fa2
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-03-15 11:33:40 -0700

    added eslint

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 
 const cors = require('cors');
-const { connect } = require('mongoose');
 const port = process.env.PORT || 5000;
 
 connectDB();
